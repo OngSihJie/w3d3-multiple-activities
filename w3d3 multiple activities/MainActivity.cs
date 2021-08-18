1cb793c97142c179777a1b3d227572ea317eb033
Sihjie ONG <1813899@nzse.ac.nz>
2021-08-19 11:24:54 +1200

    Add project files.

---
+++ w3d3 multiple activities/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace w3d3_multiple_activities
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btnNext;
+        TextView txtName;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            txtName = FindViewById<TextView>(Resource.Id.txtName);
+            btnNext = FindViewById<Button>(Resource.Id.btnNext);
+            btnNext.Click += BtnNext_Click;
+        }
+
+        private void BtnNext_Click(object sender, System.EventArgs e)
+        {
+            var activity2 = new Intent(this, typeof(SecondActivity));
+
+            activity2.PutExtra("Name", txtName.Text);
+            activity2.PutExtra("ID", 101);
+            activity2.PutExtra("Course", "Mobile Development ");
+
+            StartActivity(activity2);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ w3d3 multiple activities/SecondActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace w3d3_multiple_activities
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+        TextView txtMessage;
+        TextView txtIDMessage;
+        TextView txtCourseMessage;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.SecondActivity);
+
+            txtMessage = FindViewById<TextView>(Resource.Id.txtMessage);
+            txtMessage.Text = "Hi " + Intent.GetStringExtra("Name");
+
+            txtIDMessage = FindViewById<TextView>(Resource.Id.txtIDMessage);
+            txtIDMessage.Text = (Intent.GetIntExtra("ID", 0)).ToString();
+
+            txtCourseMessage = FindViewById<TextView>(Resource.Id.txtCourseMessage);
+            txtCourseMessage.Text = Intent.GetStringExtra("Course");
+
+
+        }
+    }
+}
